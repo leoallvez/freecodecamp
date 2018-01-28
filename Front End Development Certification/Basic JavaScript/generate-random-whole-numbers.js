b19fa4dac479efb50fb15803dea9265e04642fd7
leoallvez <leoallvez@hotmail.com>
2018-01-27 23:28:33 -0200

    first commit

---
+++ Front End Development Certification/Basic JavaScript/generate-random-whole-numbers.js
@@ -0,0 +1,8 @@
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 10.0);
+
+function randomWholeNum() {
+
+    // Only change code below this line.  
+    return Math.floor(parseInt(randomNumberBetween0and19, (Math.random())));
+}
